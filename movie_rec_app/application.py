eb37c8a74c52637edea0dc346fe757efda7523d0
Paul Wlodkowski <pawlodkowski@gmail.com>
2020-10-23 08:15:53 +0200

    black formatting

--- movie_rec_app/application.py
+++ movie_rec_app/application.py
@@ -2,27 +2,27 @@ from flask import Flask, render_template, request, flash
 from movie_rec_app.recommender import random_recommender
 
 app = Flask(__name__)
-app.secret_key = b'need_this_for_message_flashing'
+app.secret_key = b"need_this_for_message_flashing"
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/recommender')
+@app.route("/recommender")
 def recommender():
-    return render_template('recommender.html')
+    return render_template("recommender.html")
 
 
-@app.route('/results')
+@app.route("/results")
 def results():
     user_input = request.args
 
     movie_list = random_recommender(6, user_input)
 
-    if user_input['pun'] == 'Yes':
+    if user_input["pun"] == "Yes":
         message = "Great! You're gonna love these recommendations!"
         status = "pun-success"
     else:
@@ -31,8 +31,8 @@ def results():
 
     flash(message)
 
-    return render_template('results.html', movies=movie_list, status=status)
+    return render_template("results.html", movies=movie_list, status=status)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
