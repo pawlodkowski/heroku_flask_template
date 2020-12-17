5006f7b81056bce8ff9a460fa82c776f9b827308
Paul Wlodkowski <pawlodkowski@gmail.com>
2020-12-17 16:01:40 +0100

    generic spice puns

--- movie_rec_app/recommender.py
+++ movie_rec_app/recommender.py
@@ -4,18 +4,15 @@ from movie_rec_app.movies import MOVIES
 
 def random_recommender(num, user_input):
 
-    """Here is where we take the user input from Flask
-    and use it as an input to our NMF model.
+    """
+    Note
+    -----
+    At the moment this is just a dummy function for demonstration
+    purposes. The "user_input" argument is currently not used.
 
-    Note: user_input argument is currently not used."""
-
-    ### NOTE: the following is complete pseudocode
-    ### (we haven't built the functions yet)
-    """user_vector = process_user_input(movie_list, ratings_list)
-    nmf_model = load_trained_model('nmf_model.bin')
-    user_profile = nmf_model.transform(user_vector)
-    user_results = np.dot(user_profile, nmf_model.components_)
-    user_results_final = convert_to_names(user_results)"""
+    Here is where we would eventually take the user input
+    from the form and eventually use it as an input to our NMF model.
+    """
 
     user_results_final = random.sample(MOVIES, k=num)
     return user_results_final
